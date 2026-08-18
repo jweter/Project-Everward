3ea730889cd46846a05aef8cb0f28b9a8deb8bc7
Jeremy Weter <jeremyweter@gmail.com>
2026-08-18 10:59:05 -0500

    Add Unreal benchmark capture session component

---
+++ prototypes/rendering-benchmark/unreal/Source/EverwardBenchmark/BenchmarkCaptureSessionComponent.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BenchmarkCaptureSessionComponent.generated.h"
+
+class ABenchmarkAdapter;
+
+UCLASS(ClassGroup = (Everward), meta = (BlueprintSpawnableComponent))
+class EVERWARDBENCHMARK_API UBenchmarkCaptureSessionComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UBenchmarkCaptureSessionComponent();
+
+    virtual void BeginPlay() override;
+    virtual void TickComponent(
+        float DeltaTime,
+        ELevelTick TickType,
+        FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+    static constexpr double WarmupSeconds = 5.0;
+
+    TWeakObjectPtr<ABenchmarkAdapter> Adapter;
+    double WarmupElapsedSeconds = 0.0;
+    double CaptureElapsedSeconds = 0.0;
+    bool bCapturing = false;
+    bool bCompleted = false;
+    TArray<double> CpuGameThreadSamplesMs;
+    uint64 PeakProcessPhysicalBytes = 0;
+
+    void BeginCanonicalCapture();
+    void FinishCanonicalCapture();
+    bool WriteObservation() const;
+    TSharedPtr<class FJsonObject> BuildObservation() const;
+
+    static double Mean(const TArray<double>& Values);
+    static double Percentile(const TArray<double>& SortedValues, double Fraction);
+};
