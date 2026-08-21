e5945b3caa7c34640966caedd375564ee6354a67
Jeremy Weter <jeremyweter@gmail.com>
2026-08-20 21:20:36 -0500

    build: update Unreal target rules for UE 5.8

--- prototypes/rendering-benchmark/unreal/Source/EverwardBenchmark.Target.cs
+++ prototypes/rendering-benchmark/unreal/Source/EverwardBenchmark.Target.cs
@@ -6,8 +6,8 @@ public class EverwardBenchmarkTarget : TargetRules
     public EverwardBenchmarkTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("EverwardBenchmark");
     }
 }
