e783785f796970a0933465e28f214face671df13
Jeremy Weter <jeremyweter@gmail.com>
2026-08-21 19:59:02 -0500

    Match validated Unreal target settings

--- unreal/Source/Everward.Target.cs
+++ unreal/Source/Everward.Target.cs
@@ -6,8 +6,8 @@ public class EverwardTarget : TargetRules
     public EverwardTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("Everward");
     }
 }
