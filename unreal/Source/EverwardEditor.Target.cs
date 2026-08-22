38e73a04780e47c9eec6aecc09d6e7273d51436b
Jeremy Weter <jeremyweter@gmail.com>
2026-08-21 19:59:06 -0500

    Match validated Unreal editor target settings

--- unreal/Source/EverwardEditor.Target.cs
+++ unreal/Source/EverwardEditor.Target.cs
@@ -6,8 +6,8 @@ public class EverwardEditorTarget : TargetRules
     public EverwardEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("Everward");
     }
 }
