e0d2dc9c56ff20bedc0fd139b44b6da8cea41eca
Jeremy Weter <jeremyweter@gmail.com>
2026-08-23 21:32:41 -0500

    Install Everward HUD and player controller

--- unreal/Source/Everward/EverwardGameMode.cpp
+++ unreal/Source/Everward/EverwardGameMode.cpp
@@ -1,8 +1,12 @@
 #include "EverwardGameMode.h"
 
+#include "EverwardHUD.h"
+#include "EverwardPlayerController.h"
 #include "EverwardProbePawn.h"
 
 AEverwardGameMode::AEverwardGameMode()
 {
     DefaultPawnClass = AEverwardProbePawn::StaticClass();
+    HUDClass = AEverwardHUD::StaticClass();
+    PlayerControllerClass = AEverwardPlayerController::StaticClass();
 }
